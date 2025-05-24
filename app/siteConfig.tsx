e41cd62d47c00ce85162ea133a829d5bb0070ed6
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-05-24 11:42:44 +0100

    feat: added site-config

---
+++ app/siteConfig.tsx
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "troott",
+    url: "https://troott.com",
+    description: "The modern way to listen favourite sermons.",
+    baseLinks: {
+      home: "/",
+    },
+  }
+  
+  export type siteConfig = typeof siteConfig
+  
