be7712c7080c66c104b885a0df8d8782ad39a2cd
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-05-24 12:24:41 +0100

    refactor: modified global css to contain matter fonts

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 import { siteConfig } from "./siteConfig"
 import { Button } from "@/components/Button"
-import { DatabaseLogo } from "../../public/DatabaseLogo"
+import { TroottLogo } from "@/public/TroottLogo"
 import { ArrowAnimated } from "@/components/ui/ArrowAnimated"
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <Link href={siteConfig.baseLinks.home}>
-        <DatabaseLogo className="mt-6 h-10" />
+        <TroottLogo className="mt-6 h-10" />
       </Link>
       <p className="mt-6 text-4xl font-semibold text-indigo-600 sm:text-5xl dark:text-indigo-500">
         404
