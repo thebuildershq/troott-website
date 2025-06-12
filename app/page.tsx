009d67344394f8e661081a34e8a6f39af628c2fc
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-06-12 08:27:29 +0100

    feat: added bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/components/containers/Hero";
 import Mission from "@/components/containers/Mission";
 import FeatureSection16 from "@/components/containers/Preview";
 import TextSection from "@/components/containers/TextSection";
+import { BentoDemo } from "@/components/containers/UserSection";
 
 
 
@@ -12,6 +13,7 @@ export default function Home() {
     <main className="flex flex-col overflow-hidden">
       <HeroSection/>
       
+      <BentoDemo  />
       
       <FeatureSection16/>
 
