aefd962dd12a7ad73e838bc4a150b292309a34da
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-07-03 21:08:15 +0100

    refactor: modified hero-image

--- components/ui/HeroImage.tsx
+++ components/ui/HeroImage.tsx
@@ -5,7 +5,7 @@ import ThemedImage from "../containers/ThemedImage"
 export default function HeroImage() {
   return (
     <section aria-label="Hero Image of the website" className="flow-root">
-      {/* <div className="rounded-2xl bg-slate-50/40 p-2 ring-1 ring-inset ring-slate-200/50 dark:bg-gray-900/70 dark:ring-white/10"> */}
+     
         <div className="rounded-xl bg-white ring-1 ring-slate-900/5 dark:bg-slate-950 dark:ring-white/15">
           <ThemedImage
             lightSrc="/images/hero-image.png"
@@ -16,7 +16,7 @@ export default function HeroImage() {
             className="rounded-xl shadow-2xl dark:shadow-neutral-600/10"
           />
         </div>
-      {/* </div> */}
+      
     </section>
   )
 }
